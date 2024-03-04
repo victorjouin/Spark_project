b49d59f45cf1b33e721cf7324478ae3b771687a5
victor jouin <77397820+victorjouin@users.noreply.github.com>
2024-03-04 14:36:28 +0100

    test udf

---
+++ src/fr/hymaia/exo4/python_udf.py
@@ -0,0 +1,31 @@
+import pyspark.sql.functions as f
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType, IntegerType
+import time
+
+# Create SparkSession
+spark = SparkSession.builder.appName("exo4").master("local[*]").getOrCreate()
+
+# Define Python UDF
+def category_name(category):
+    if int(category) < 6:
+        return 'food'
+    else:
+        return 'furniture'
+
+# Register UDF
+category_name_udf = udf(category_name, StringType())
+
+# Load data
+df = spark.read.csv('./src/resources/exo4/sell.csv', header=True)
+start_time = time.time()
+# Convert 'category' column to IntegerType
+df = df.withColumn('category', df['category'].cast(IntegerType()))
+
+# Use UDF to add new column
+df = df.withColumn('category_name', category_name_udf(df['category']))
+
+df.show()
+end_time = time.time()
+print("python Execution time: {} seconds".format(end_time - start_time))
--- src/fr/hymaia/exo4/scala_udf.py
+++ src/fr/hymaia/exo4/scala_udf.py
@@ -1,9 +1,31 @@
-import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
+from pyspark.sql.column import Column, _to_java_column, _to_seq
+from pyspark.sql.types import IntegerType
+import time
 
+# Create SparkSession with the JAR file in the configuration
+spark = SparkSession.builder \
+    .appName("exo4") \
+    .master("local[*]") \
+    .config('spark.jars', './src/resources/exo4/udf.jar') \
+    .getOrCreate()
 
-spark = SparkSession.builder.appName("exo4").master("local[*]").getOrCreate()
+# Define Python function to wrap the Scala UDF
+def category_name(col):
+    sc = spark.sparkContext
+    add_category_name_udf = sc._jvm.fr.hymaia.sparkfordev.udf.Exo4.addCategoryNameCol()
+    return Column(add_category_name_udf.apply(_to_seq(sc, [col], _to_java_column)))
 
-def main():
-    print("Hello world!")
+# Load data
+df = spark.read.csv('./src/resources/exo4/sell.csv', header=True)
 
+start_time = time.time()
+# Convert 'category' column to IntegerType
+df = df.withColumn('category', df['category'].cast(IntegerType()))
+
+# Use UDF to add new column
+df = df.withColumn('category_name', category_name(df['category']))
+
+df.show()
+end_time = time.time()
+print(" scala Execution time: {} seconds".format(end_time - start_time))
